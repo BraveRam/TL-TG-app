3a4491b1ef4ba05433af25e5479049d5cf4928dc
Braveram <lenchomengistu100@gmail.com>
2024-09-06 08:03:40 +0300

    ntn

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ function App(){
       return (
         <div className="mx-auto block w-[330px]">
           <h1 className="text-center text-2xl mt-5 font-extrabold text-red-500">🤖Bot kana fayyadamuuf #channel keenya #Join gochuu qabdu! San booda #page kana #reload ykn cufaa banaa👇</h1>
-          <Link className="text-center mx-auto block py-2 px-4 rounded bg-blue-500 text-white mt-3" to="https://t.me/Oro_Tech_Tipz">JOIN CHANNEL</Link>
+          <Link className="text-center mx-auto block font-extrabold py-2 px-4 rounded bg-blue-500 text-white mt-3" to="https://t.me/Oro_Tech_Tipz">JOIN CHANNEL</Link>
     </div>
         )
     }
   } else{
     return <div className="mx-auto block w-[330px]">
         <h1 className="text-center text-2xl mt-5 font-extrabold text-red-500">Open the bot in Telegram</h1>
-        <Link className="text-center py-2 px-4 mx-auto block rounded bg-blue-500 text-white mt-3" to="https://t.me/OromoTranslatorBot">CLICK HERE</Link>
+        <Link className="text-center py-2 px-4 mx-auto font-extrabold block rounded bg-blue-500 text-white mt-3" to="https://t.me/OromoTranslatorBot">CLICK HERE</Link>
     </div>
   }
 }
