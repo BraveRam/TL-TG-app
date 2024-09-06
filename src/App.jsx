fff770e353ec05f0c7b0b599dac1628b476d25e5
BraveRam <99811385+BraveRam@users.noreply.github.com>
2024-09-06 07:44:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App(){
   )
     } else{
       return (
-        <div className="mx-auto block">
+        <div className="mx-auto block w-[330px]">
           <h1 className="text-center text-2xl mt-5 font-extrabold text-red-500">🤖Bot kana fayyadamuuf #channel keenya #Join gochuu qabdu👇</h1>
           <Link className="text-center mx-auto block py-2 px-4 rounded bg-blue-500 text-white mt-3" to="https://t.me/Oro_Tech_Tipz">JOIN CHANNEL</Link>
     </div>
         )
     }
   } else{
-    return <div className="mx-auto block">
+    return <div className="mx-auto block w-[330px]">
         <h1 className="text-center text-2xl mt-5 font-extrabold text-red-500">Open the bot in Telegram</h1>
         <Link className="text-center py-2 px-4 mx-auto block rounded bg-blue-500 text-white mt-3" to="https://t.me/OromoTranslatorBot">CLICK HERE</Link>
     </div>
