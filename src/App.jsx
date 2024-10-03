56929ea46e8638688049df4e2f1ffe4ef07e7882
BraveRam <99811385+BraveRam@users.noreply.github.com>
2024-10-03 08:17:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,13 @@ function App() {
   }, []);
 
   if (isLoading) {
-    return <div className="text-center text-2xl mt-5 font-extrabold">Loading...</div>;
+    return <div className="flex items-center justify-center h-screen">
+      <h1 className="animate-spin text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" height="88px" viewBox="0 -960 960 960" width="88px" fill="#e8eaed"><path d="M196-331q-20-36-28-72.5t-8-74.5q0-131 94.5-225.5T480-798h43l-80-80 39-39 149 149-149 149-40-40 79-79h-41q-107 0-183.5 76.5T220-478q0 29 5.5 55t13.5 49l-43 43ZM476-40 327-189l149-149 39 39-80 80h45q107 0 183.5-76.5T740-479q0-29-5-55t-15-49l43-43q20 36 28.5 72.5T800-479q0 131-94.5 225.5T480-159h-45l80 80-39 39Z"/></svg>
+      </h1>
+    </div>
   }
 
-  // Once loading is done, conditionally render based on state
   if (state.isInitialized) {
     if (state.hasAccess) {
       return (
@@ -37,7 +40,7 @@ function App() {
             🤖 Bot kana fayyadamuuf #channel keenya #Join gochuu qabdu! San booda #page kana #reload ykn cufaa banaa👇
           </h1>
           <Link
-            className="text-center mx-auto block font-extrabold py-2 px-4 rounded bg-blue-500 text-white mt-3"
+            className="text-center mx-auto block font-extrabold py-2 px-4 rounded bg-blue-500 text-white mt-3 animate-pulse"
             to="https://t.me/Oro_Tech_Tipz"
           >
             JOIN CHANNEL
@@ -52,7 +55,7 @@ function App() {
           Open the bot in Telegram
         </h1>
         <Link
-          className="text-center py-2 px-4 mx-auto font-extrabold block rounded bg-blue-500 text-white mt-3"
+          className="text-center py-2 px-4 mx-auto font-extrabold block rounded bg-blue-500 text-white mt-3 animate-pulse"
           to="https://t.me/OromoTranslatorBot"
         >
           CLICK HERE
